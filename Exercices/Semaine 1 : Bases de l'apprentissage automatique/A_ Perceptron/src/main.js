bfff458d2700ac1aef275f5b7755d9935b14990d
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2019-03-11 21:28:11 +0100

    Ajout de commentaires

--- Exercices/Semaine 1 : Bases de l'apprentissage automatique/A_ Perceptron/src/main.js
+++ Exercices/Semaine 1 : Bases de l'apprentissage automatique/A_ Perceptron/src/main.js
@@ -20,25 +20,10 @@ let weights, bias;
  * Programme principal
  */
 
+/*
+ * Entrâinement du réseau
+ */
 [weights, bias] = train(Data.dataset, Data.classes, 0.1, 5);
 
 console.log(`Poids : ${weights}`);
 console.log(`Biais : ${bias}`);
-
-
-
-for (let i of Data.dataset.keys()) {
-  /*
-  prediction = predict(Data.dataset[i], Data.weights, Data.bias);
-  console.log(`Attendu : ${Data.classes[i]} – Prédit : ${prediction}`);
-// console.log(Data.classes[i]);
-  row = document.createElement('tr');
-  correct = document.createElement('td');
-  correct.innerHTML = Data.classes[i];
-  predicted = document.createElement('td');
-  predicted.innerHTML = prediction;
-  row.appendChild(correct);
-  row.appendChild(predicted);
-  container.appendChild(row);
-  */
-}
